af488b1ef40ab0804f4829d9475e07a706969b7c
alinbsp <alin@fingolf.in>
2015-09-18 17:36:39 +0300

    Handle numbers and strings

--- subterfuge.hs
+++ subterfuge.hs
@@ -1,6 +1,42 @@
 module Main where
-import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad (liftM)
 import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+main :: IO ()
+main = do
+     args <- getArgs
+     putStrLn (readExpr (args !! 0))
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+            char '"'
+            s <- many (noneOf "\"")
+            char '"'
+            return $ String s
+
+parseAtom :: Parser LispVal
+parseAtom = do
+          first <- letter <|> symbol
+          rest <- many (letter <|> digit <|> symbol)
+          let atom = first:rest
+          return $ case atom of
+                 "#t" -> Bool True
+                 "#f" -> Bool False
+                 _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -9,30 +45,6 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value"
-
-
--- Basic return values
-data LispVal = Atom String
-            | List [LispVal]
-            | DottedList [LispVal] LispVal
-            | Number Integer
-            | String String
-            | Bool Bool
-
-parseAtom :: Parser LispVal
-parseAtom = do
-    first <- letter <|> symbol
-    rest <- (letter <|> digit <|> symbol)
-    let atom = first:rest
-    return $ case atom of
-        "#t" -> Bool True
-        "#f" -> Bool False
-        _    -> Atom atom
-
-main :: IO ()
-main = do
-    (expr:_) <- getArgs
-    putStrLn (readExpr expr)
+readExpr input = case parse parseExpr "lisp" input of
+         Left err -> "No match: " ++ show err
+         Right val -> "Found value"
